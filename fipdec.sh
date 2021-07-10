ef6221e09f9cc78e3e15495a330416ea0e47f000
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-07-10 07:06:52 +0700

    Create fipdec.sh

--- fipdec.sh
+++ fipdec.sh
@@ -15,7 +15,7 @@ echo 'Result :'
 sort -n "$target" | sed '/#/d' && echo # sed -e "s/[^ a-zA-Z1-9']//g" -e 's/ \+/ /' && echo
 echo "Total Line : $(wc -l $target | sed 's/[^1-9]//g')"
 echo "========================================="
-echo 'This Tool Originally By PhynX#4912'
+echo 'This Tool Originally By PhynX#0745'
 echo
 else
 clear
