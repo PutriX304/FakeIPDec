84aa638dc8d7b2b01cb30e3090ff497c0f5b00ae
PhynX <48216256+PhynX404@users.noreply.github.com>
2021-11-04 14:45:54 +0700

    Update fipdec.sh

--- fipdec.sh
+++ fipdec.sh
@@ -2,17 +2,17 @@
 
 clear
 echo 'FakeIP Deobfuscator'
-echo
-echo
-echo "File & Directory Lists :"
-ls --group-directories-first -1 -N
+echo '==================='
+echo '           by:PhynX'
 echo
 read -p "File Name : " target
 if [[ -n $target ]] | [[ -f $target ]]
 then
 echo
-echo 'Result :'
-sort -n "$target" | sed '/#/d' && echo # sed -e "s/[^ a-zA-Z1-9']//g" -e 's/ \+/ /' && echo
+echo 'Result #1 (#):'
+sort -n "$target" | sed '/#/d' && echo && echo
+echo 'Result #2 (//):'
+sort -n "$target" | sed '/#/d' | sed -e '/^[ \t]*\/\//d' && echo
 echo "Total Line : $(wc -l $target | sed 's/[^1-9]//g')"
 echo "========================================="
 echo 'This Tool Originally By PhynX#0745'
